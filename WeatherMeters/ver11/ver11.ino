491dfc02da4d3939e674a6506e1cbd2375cffe0d
Manos Kontakis <emkontakis@gmail.com>
2014-07-29 22:07:53 +0300

    Prettying up code

--- WeatherMeters/ver11/ver11.ino
+++ WeatherMeters/ver11/ver11.ino
@@ -2,13 +2,9 @@
 #include "DHT.h"
 #include <SPI.h>
 #include <Ethernet.h>
-
-
 #include <Wire.h>
-#include <ArduinoNunchuk.h>
 #include <stdlib.h>
 #include <Servo.h>
-
 
 
 #define BMP085_ADDRESS 0x77  // I2C address of BMP085
@@ -30,12 +26,11 @@
 #define SERVO_2_PIN 41
 
 DHT dht(DHTPIN, DHTTYPE);
+//Servo involved variables
 int base_servo_position = 0;    // variable to store the servo position
 int inner_servo_position = 80;
-
 Servo myservo;  // create servo object to control a servo 
 Servo myservo2;
-
 
 // this must be unique
 byte mac[] = {
@@ -51,14 +46,13 @@ IPAddress subnet(255, 255, 255, 0);
 IPAddress server(192, 168, 1, 5); //Ip of the Computer running apache
 EthernetServer srv(8888);                             //server port     
 String readString;
+// change to your server's port
+int serverPort = 80;
 
 //Change to your domain name for virtual servers
 //char serverName[] = "www.google.com";
 // If no domain name, use the ip address above
 // char serverName[] = "74.125.227.16";
-
-// change to your server's port
-int serverPort = 80;
 
 EthernetClient client;
 int totalCount = 0;
@@ -89,27 +83,12 @@ float temperature;
 long pressure;
 float altitude;
 
+//Weather Meters Global Variables
 double windSpeed;
 double gust;
 double dir;
 double knots;
 double rain;
-
-
-
-ulong nextCalcGust;
-ulong nextCalcSpeed;                // When we next calc the wind speed
-ulong nextCalcDir;                  // When we next calc the direction
-ulong nextCalcRain;                  // When we next calc the rain drop
-ulong time;                         // Millis() at each start of loop().
-
-#define MSECS_CALC_WIND_SPEED 60000
-#define MSECS_CALC_GUST 20000
-#define MSECS_CALC_WIND_DIR   10000
-#define MSECS_CALC_RAIN_FALL  90000
-
-int gitEeeex;
-
 
 void setup() {
   Serial.begin(9600);
@@ -122,7 +101,6 @@ void setup() {
 
   // If using dhcp, comment out the line above
   // and uncomment the next 2 lines
-
   // if(!Ethernet.begin(mac)) Serial.println(F("failed"));
   // else Serial.println(F("ok"));
   digitalWrite(10, HIGH);
@@ -148,20 +126,16 @@ void setup() {
 //  nunchuk.init();
   myservo.attach(SERVO_1_PIN);  // attaches the servo on pin 2 to the servo object 
   myservo2.attach(SERVO_2_PIN);  
-  
-  nextCalcRain = millis() + MSECS_CALC_RAIN_FALL;
-  nextCalcSpeed = millis() + MSECS_CALC_WIND_SPEED;
-  nextCalcDir   = millis() + MSECS_CALC_WIND_DIR;
+  //Checking free ram
   Serial.print("Free ram is: ");
   Serial.println(freeRam());
   srv.begin();
   myservo.write(inner_servo_position);
-   dht.begin();
+  dht.begin();
+  //One second delay to let thing settle down
   delay(1000);
-
 }
 
-char params[32];
 // Use these for altitude conversions
 const float p0 = 101325;     // Pressure at sea level (Pa)
 const float p1 = 1013.25;     // Pressure at sea level (hPa)
@@ -172,7 +146,7 @@ void loop() {
 
   thisMillis = millis();
   thisGustMillis = millis();
-
+  //Gust measure & logging
   if (thisGustMillis - lastGustMillis > gustDelayMillis) {
     unsigned long factor = thisGustMillis - lastGustMillis;
     lastGustMillis = thisGustMillis;
@@ -194,27 +168,21 @@ void loop() {
     Serial.println(gustCount, DEC);
   }
 
-  if (thisMillis - lastMillis > delayMillis)
-  {
+  if (thisMillis - lastMillis > delayMillis) {
     unsigned long factor = thisMillis - lastMillis;
     lastMillis = thisMillis;
     Serial.println(lastMillis);
+    
+    //Getting sensor values
     value0 = analogRead(lm35_pin);
     value0 = (5.0 * value0 * 100.0) / 1024.0;
     humidity();
-        Serial.println("DEBUGggggg 1");
     temperature = (bmp085GetTemperature(bmp085ReadUT())) * 0.1;
-            Serial.println("DEBUGggggg 2");
     pressure = (bmp085GetPressure(bmp085ReadUP())) * 0.01;
-            Serial.println("DEBUGggggg 3");
     //altitude = (float)44330 * (1 - pow(((float) pressure/p0), 0.190295));
     long tempPres = bmp085GetPressure(bmp085ReadUP());
-            Serial.println("DEBUGggggg 4");
     altitude = (float)44330 * (1 - pow(((float) tempPres / p0), 0.190295));
-            Serial.println("DEBUGggggg 5");
     float alt = ((1 -  pow(((float) pressure / p1), 0.190284)) * 145366.45);
-            Serial.println("DEBUGggggg 6");
-
 
     gust =  getGust(factor);
     gust = gust * 0.868976;
@@ -222,8 +190,8 @@ void loop() {
     rain = getUnitRain();
     windSpeed = getUnitWind(factor);
     knots = windSpeed * 0.868976;
-
-
+    
+    //Printing sensor values for debugging purposes.
     Serial.println();
     Serial.print("Temperature: ");
     Serial.print(temperature, 2);
@@ -250,9 +218,11 @@ void loop() {
 
     Serial.print(F("Free ram is: "));
     Serial.println(freeRam());
-
+    
+    //Pre constructing the Get HTTP Request
+    //dtostrf function converts Double to String
     strcpy(outBuf, "GET /write3.php?value0=");
-    itoa(value0, tBuf, 10);
+    dtostrf(dht_temp, 2, 2, tBuf);
     strcat(outBuf, tBuf);
 
     strcat(outBuf, "&value1=");
@@ -261,14 +231,11 @@ void loop() {
 
     strcat(outBuf, "&value2=");
     dtostrf(temperature, 2, 2, tBuf);
-    //  itoa(temperature,tBuf,10);
-    Serial.println(tBuf);
     strcat(outBuf, tBuf);
 
     strcat(outBuf, "&value3=");
     itoa(pressure, tBuf, 10);
     strcat(outBuf, tBuf);
-
 
     strcat(outBuf, "&value4=");
     itoa(altitude, tBuf, 10);
@@ -289,38 +256,22 @@ void loop() {
     strcat(outBuf, "&value8=");
     dtostrf(knots, 2, 2, tBuf);
     strcat(outBuf, tBuf);
-
-    if (!getPage(server, serverPort, outBuf)) Serial.print(F("Fail "));
-    else Serial.print(F("Pass "));
+    
+    //Sending URL for execution.
+    if (!getPage(server, serverPort, outBuf)) {
+      Serial.print(F("Fail "));
+    } else {
+      Serial.print(F("Pass "));
+    }
+    //Printing total succesful requests.
     totalCount++;
     Serial.println(totalCount, DEC);
   }
-
-
 }
-//char *dtostrff (double val, signed char width, unsigned char prec, char *sout) {
-//  char fmt[20];
-//  sprintf(fmt, "%%%d.%df", width, prec);
-//  sprintf(sout, fmt, val);
-//  return sout;
-//
-//}
 
-//void wiiNunchuk() {
-//  nunchuk.update();
-//  int analogX = nunchuk.analogX;
-//  int analogY = nunchuk.analogY;
-//  analogX = map(analogX, 0, 255, 0, 179);
-//  analogY = map(analogY, 0, 255, 0, 179);
-//  Serial.println(analogX);
-//  myservo2.write(analogX);
-//  myservo.write(analogY);
-//  delay(30);
-//
-//}
-
+//Humidity handler function
 void humidity() {
-    // Reading temperature or humidity takes about 250 milliseconds!
+  // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   dht_humidity = dht.readHumidity();
   // Read temperature as Celsius
@@ -340,77 +291,34 @@ void humidity() {
 }
 
 
-
-
+//Debugging function. Returns the free Storage Space.
 int freeRam() {
   extern int __heap_start, *__brkval;
   int v;
   return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int) __brkval);
 }
 
-byte getPage(IPAddress ipBuf, int thisPort, char *outBuf)
-{
-
+//Method that executes the precontructed (outBuf) HTTP request
+byte getPage(IPAddress ipBuf, int thisPort, char *outBuf) {
   int inChar;
-
   Serial.print(F("connecting..."));
 
-  if (client.connect(ipBuf, thisPort))
-  {
+  if (client.connect(ipBuf, thisPort)) {
+	//Executing HTTP Request (GET)
     Serial.println(F("connected"));
-
     Serial.print(F("outBuf length = "));
     Serial.println(strlen(tBuf), DEC);
-
-    Serial.println("DEBUG___2");
     Serial.print(F("outBuf length = "));
     Serial.println(strlen(outBuf), DEC);
+	
     client.write(outBuf);
-    Serial.println("DEBUG___3");
     client.println(" HTTP/1.1");
-    Serial.println("DEBUG___4");
-    client.println("Host: 192.168.1.5");
-    Serial.println("DEBUG___5");
+    client.println("Host: 192.168.1.5");;
     client.println("Connection: close");
     client.println();
 
-
-    //    client.print("GET /write3.php?value0=");  //GET to php file which handle data (value0 and value1) into a database
-    //    //Temperature
-    //    client.print(value0);
-    //    //Humidity
-    //    client.print("&value1=");
-    //    client.print(dht_humidity);
-    //    //bmp085 values
-    //    client.print("&value2=");
-    //    client.print(temperature);
-    //
-    //    client.print("&value3=");
-    //    client.print(pressure);
-    //
-    //    client.print("&value4=");
-    //    client.print(altitude);
-    //
-    //    //Weather Meters (Wind speed, direction, rain)
-    //    client.print("&value5=");
-    //    client.print(gust); //Wind speed every 20 seconds
-    //
-    //    client.print("&value6=");
-    //    client.print(dir);
-    //
-    //    client.print("&value7=");
-    //    client.print(rain);
-    //
-    //    client.print("&value8=");
-    //    client.print(knots); //Wind speed every 60 seconds
-    //
-    //      client.println(" HTTP/1.1");
-    //    client.println("Host: 192.168.1.3");
-    //    client.println("Connection: close");
-    //    client.println();
   }
-  else
-  {
+  else {
     Serial.println(F("failed"));
     return 0;
   }
@@ -418,21 +326,17 @@ byte getPage(IPAddress ipBuf, int thisPort, char *outBuf)
   // connectLoop controls the hardware fail timeout
   int connectLoop = 0;
 
-  while (client.connected())
-  {
-    while (client.available())
-    {
+  while (client.connected()) {
+    while (client.available()) {
       inChar = client.read();
       Serial.write(inChar);
       // set connectLoop to zero if a packet arrives
       connectLoop = 0;
     }
-
     connectLoop++;
 
     // if more than 10000 milliseconds since the last packet
-    if (connectLoop > 10000)
-    {
+    if (connectLoop > 10000) {
       // then close the connection from this end.
       Serial.println();
       Serial.println(F("Timeout"));
@@ -472,15 +376,10 @@ int md;
 long b5;
 
 
-
-
-
-
 // Stores all of the bmp085's calibration values into global variables
 // Calibration values are required to calculate temp and pressure
 // This function should be called at the beginning of the program
-void bmp085Calibration()
-{
+void bmp085Calibration() {
   ac1 = bmp085ReadInt(0xAA);
   ac2 = bmp085ReadInt(0xAC);
   ac3 = bmp085ReadInt(0xAE);
@@ -492,13 +391,11 @@ void bmp085Calibration()
   mb = bmp085ReadInt(0xBA);
   mc = bmp085ReadInt(0xBC);
   md = bmp085ReadInt(0xBE);
-
 }
 
 // Calculate temperature given ut.
 // Value returned will be in units of 0.1 deg C
-short bmp085GetTemperature(unsigned int ut)
-{
+short bmp085GetTemperature(unsigned int ut) {
   long x1, x2;
 
   x1 = (((long)ut - (long)ac6) * (long)ac5) >> 15;
@@ -512,8 +409,7 @@ short bmp085GetTemperature(unsigned int ut)
 // calibration values must be known
 // b5 is also required so bmp085GetTemperature(...) must be called first.
 // Value returned will be pressure in units of Pa.
-long bmp085GetPressure(unsigned long up)
-{
+long bmp085GetPressure(unsigned long up) {
   long x1, x2, x3, b3, b6, p;
   unsigned long b4, b7;
 
@@ -545,8 +441,7 @@ long bmp085GetPressure(unsigned long up)
 }
 
 // Read 1 byte from the BMP085 at 'address'
-char bmp085Read(unsigned char address)
-{
+char bmp085Read(unsigned char address) {
   unsigned char data;
 
   Wire.beginTransmission(BMP085_ADDRESS);
@@ -554,17 +449,15 @@ char bmp085Read(unsigned char address)
   Wire.endTransmission();
 
   Wire.requestFrom(BMP085_ADDRESS, 1);
-  while (!Wire.available())
-    ;
-
+  while (!Wire.available());
+    
   return Wire.read();
 }
 
 // Read 2 bytes from the BMP085
 // First byte will be from 'address'
 // Second byte will be from 'address'+1
-int bmp085ReadInt(unsigned char address)
-{
+int bmp085ReadInt(unsigned char address) {
   unsigned char msb, lsb;
 
   Wire.beginTransmission(BMP085_ADDRESS);
@@ -572,16 +465,15 @@ int bmp085ReadInt(unsigned char address)
   Wire.endTransmission();
 
   Wire.requestFrom(BMP085_ADDRESS, 2);
-  while (Wire.available() < 2)
-    ;
+  while (Wire.available() < 2);
+    
   msb = Wire.read();
   lsb = Wire.read();
   return (int) msb << 8 | lsb;
 }
 
 // Read the uncompensated temperature value
-unsigned int bmp085ReadUT()
-{
+unsigned int bmp085ReadUT() {
   unsigned int ut;
 
   // Write 0x2E into Register 0xF4
@@ -600,8 +492,7 @@ unsigned int bmp085ReadUT()
 }
 
 // Read the uncompensated pressure value
-unsigned long bmp085ReadUP()
-{
+unsigned long bmp085ReadUP() {
   unsigned char msb, lsb, xlsb;
   unsigned long up = 0;
 
@@ -635,50 +526,40 @@ unsigned long bmp085ReadUP()
 
 
 
-
-
-
-static int vaneValues[] PROGMEM =      {
+//Vane Values and corresponding Directions.
+static int vaneValues[] PROGMEM = {
   66, 84, 92, 127, 184, 244, 287, 406, 461, 600, 631, 702, 786, 827, 889, 946
 };
 static int vaneDirections[] PROGMEM = {
   1125, 675, 900, 1575, 1350, 2025, 1800, 225, 450, 2475, 2250, 3375, 0, 2925, 3150, 2700
 };
 
-double getWindVane()
-{
-  analogReference(DEFAULT);
+double getWindVane() {
+  analogReference(DEFAULT); //Powering the vane just before it is used
   digitalWrite(VANE_PWR, HIGH);
   delay(100);
-  for (int n = 0; n < 10; n++)
-  {
+  for (int n = 0; n < 10; n++) {
     analogRead(VANE_PIN);
   }
 
   unsigned int reading = analogRead(VANE_PIN);
-  //Serial.println("");
-  //Serial.println(reading);
   digitalWrite(VANE_PWR, LOW);
   unsigned int lastDiff = 2048;
 
-  for (int n = 0; n < 16; n++)
-  {
+  for (int n = 0; n < 16; n++) {
     int diff = reading - pgm_read_word(&vaneValues[n]);
     diff = abs(diff);
     //Serial.println(diff);
     if (diff == 0)
       return pgm_read_word(&vaneDirections[n]) / 10.0;
 
-    if (diff > lastDiff)
-    {
+    if (diff > lastDiff) {
       return pgm_read_word(&vaneDirections[n - 1]) / 10.0;
     }
 
     lastDiff = diff;
   }
-
   return pgm_read_word(&vaneDirections[15]) / 10.0;
-
 }
 
 
@@ -686,11 +567,8 @@ double getWindVane()
 #define TEST_PAUSE 60000
 
 volatile unsigned long anem_count = 0;
-volatile unsigned long anem_count_2 = 0;
-volatile unsigned long anem_count_3 = 0;
 volatile unsigned long anem_last = 0;
 volatile unsigned long anem_min = 0xffffffff;
-
 
 
 double getUnitWind(unsigned long factor) {
@@ -700,17 +578,16 @@ double getUnitWind(unsigned long factor) {
   return result;
 }
 
-
 unsigned long thisMillis_2 = 0;
 unsigned long lastMillis_2 = 0;
 
-double getGust(unsigned long factor)
-{
+double getGust(unsigned long factor) {
   unsigned long reading = anem_count;
   anem_count = 0;
   double result = (WIND_FACTOR * reading) / (factor / 1000);
   return result;
 }
+
 void anemometerClick() {
   long thisTime = micros() - anem_last;
   anem_last = micros();
@@ -729,8 +606,7 @@ void anemometerClick() {
 volatile unsigned long rain_count = 0;
 volatile unsigned long rain_last = 0;
 
-double getUnitRain()
-{
+double getUnitRain() {
 
   unsigned long reading = rain_count;
   rain_count = 0;
@@ -738,19 +614,16 @@ double getUnitRain()
   return unit_rain;
 }
 
-void rainGageClick()
-{
+void rainGageClick() {
   long thisTime = micros() - rain_last;
   rain_last = micros();
-  if (thisTime > 500)
-  {
+  if (thisTime > 500) {
     rain_count++;
   }
 }
 
-
-double dewPoint(double celsius, double humidity)
-{
+//Calculating Dew Point the classic way. (Resource - heavy function)
+double dewPoint(double celsius, double humidity) {
   // (1) Saturation Vapor Pressure = ESGG(T)
   double RATIO = 373.15 / (273.15 + celsius);
   double RHS = -7.90298 * (RATIO - 1);
@@ -770,8 +643,7 @@ double dewPoint(double celsius, double humidity)
 // delta max = 0.6544 wrt dewPoint()
 // 6.9 x faster than dewPoint()
 // reference: http://en.wikipedia.org/wiki/Dew_point
-double dewPointFast(double celsius, double humidity)
-{
+double dewPointFast(double celsius, double humidity) {
   double a = 17.271;
   double b = 237.7;
   double temp = (a * celsius) / (b + celsius) + log(humidity * 0.01);
@@ -831,22 +703,20 @@ void servoControl() {
            //controls the Arduino if you press the buttons
 
            //Base Servo
-           if (readString.indexOf("?button1on") >0){
-                for(base_servo_position = 0; base_servo_position < 160; base_servo_position += 3)  // goes from 0 degrees to 180 degrees 
-                {                                  // in steps of 1 degree 
+           if (readString.indexOf("?button1on") >0) {
+                for(base_servo_position = 0; base_servo_position < 160; base_servo_position += 3) {  // goes from 0 degrees to 180 degrees                                   // in steps of 1 degree 
                   myservo2.write(base_servo_position);              // tell servo to go to position in variable 'pos' 
                   delay(15);                       // waits 15ms for the servo to reach the position 
                 } 
            }
-           if (readString.indexOf("?button1off") >0){
-                for(base_servo_position = 160; base_servo_position>=1; base_servo_position-=3)     // goes from 180 degrees to 0 degrees 
-                {                                
+           if (readString.indexOf("?button1off") >0) {
+                for(base_servo_position = 160; base_servo_position>=1; base_servo_position-=3) {    // goes from 180 degrees to 0 degrees                                
                   myservo2.write(base_servo_position);              // tell servo to go to position in variable 'pos' 
                   delay(15);                       // waits 15ms for the servo to reach the position 
                 } 
            }
            //Inner Servo
-           if (readString.indexOf("?button2on") >0){
+           if (readString.indexOf("?button2on") > 0) {
                if (inner_servo_position >= 1 && inner_servo_position <= 170) {
                     if (inner_servo_position > 160) {
                       inner_servo_position -= 10;
@@ -857,7 +727,7 @@ void servoControl() {
                     delay(15);                       // waits 15ms for the servo to reach the position 
                }
            }
-           if (readString.indexOf("?button2off") >0){
+           if (readString.indexOf("?button2off") >0) {
                   if (inner_servo_position >= 1 && inner_servo_position <= 170) {
                     if (inner_servo_position > 10) {
                       inner_servo_position -= 10;
